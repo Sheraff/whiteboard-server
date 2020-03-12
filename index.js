a215808a930a0f5409c5dbb0c0efdc3e2f5f898b
Sheraff <fpellet@ensc.fr>
2020-03-13 00:42:28 +0100

    various cleanups and todos after reading some docs

--- index.js
+++ index.js
@@ -30,10 +30,11 @@ const server = http2.createSecureServer({
 	cert: fs.readFileSync('./server/ssl/localhost-cert.pem'),
 })
 
-server.on('session', (session) => {
-	session.on('stream', onStream)
-	session.setTimeout(TIMEOUT, () => void session.close(NGHTTP2_CANCEL))
-})
+server.on('stream', onStream)
+
+// server.on('session', (session) => {
+// 	session.setTimeout(TIMEOUT, () => void session.close(NGHTTP2_CANCEL))
+// })
 
 server.listen(HTTPS_PORT)
 
@@ -60,6 +61,7 @@ function onStream(stream, headers) {
 
 function respondToStreamError(err, stream) {
 	console.log(err);
+	// most likely a NGHTTP2_REFUSED_STREAM sent by the client because pushed resource is not needed (already cached)
 	if (err.code === 'ENOENT') {
 		stream.respond({ [HTTP2_HEADER_STATUS]: HTTP_STATUS_NOT_FOUND });
 	} else {
@@ -69,34 +71,58 @@ function respondToStreamError(err, stream) {
 }
 
 function push(stream, filePath) {
-	stream.pushStream({ [HTTP2_HEADER_PATH]: filePath }, { parent: stream.id }, (err, pushStream, headers) => {
+	stream.pushStream(
+		{ [HTTP2_HEADER_PATH]: filePath }, 
+		{ parent: stream.id }, 
+		(err, pushStream, headers) => {
 
-		// swallow error b/c abrupt interrupt shouldn't kill server
-		pushStream.on('error', err => void console.log('pushStream', err))
+			// swallow error b/c abrupt interrupt shouldn't kill server
+			pushStream.on('error', err => void console.log('pushStream', err))
 
-		pushStream.respondWithFile(
-			path.join(PUBLIC_ROOT, filePath),
-			{ 'content-type': mime.lookup(filePath) },
-			{ onError: respondToStreamError }
-		)
-	})
+			pushStream.respondWithFile(
+				path.join(PUBLIC_ROOT, filePath),
+				{ 'content-type': mime.lookup(filePath) },
+				{ onError: respondToStreamError }
+			)
+		}
+	)
 }
 
 function respondWithIndex(stream) {
+	// in theory, a PUSH_PROMISE should be sent before the response to the request (or at least the push promise header frame)
+	// https://developers.google.com/web/fundamentals/performance/http2#push_promise_101
+
+	// pushing resources might benefit from being split up more (for example, sw scripts pushed when sw.js is requested)
+	// using `<link preload>` in index.html head will help requesting only what's not in cache (and push associated resources then)
+
+	// Push resources in evaluation-dependence order.
+
+	// if possible, try to push less bytes than available in congestion window (including final stream response)
+	// use stream.localWindowSize or stream.remoteWindowSize for this?
+	// would require maintining a table of file sizes (built on server startup)
+
+	if (stream.pushAllowed)
+		INDEX_PUSH.forEach(path => push(stream, path))
 	makeIndex().then(body => {
 		stream.respond({ 'content-type': mime.contentType('text/html') })
 		stream.end(body)
 	})
-	if (stream.pushAllowed)
-		INDEX_PUSH.forEach(path => push(stream, path))
 }
 
 function respondWithAnyFile(stream, headers) {
 	let requestedPath = decodeURI(headers[HTTP2_HEADER_PATH])
-	if (!requestedPath.includes('.')) {
-		const bits = requestedPath.split('/')
-		const fileName = `graphs_${bits.pop()}.svg`
-		requestedPath = path.join(...bits, '/graphs', fileName)
+	const { ext, name, dir } = path.parse(requestedPath)
+
+	if(dir === '/static') {
+		// TODO: this is to prevent crashes, but these requests shouldn't reach the server
+		console.log('requested static', requestedPath)
+		stream.respond({ [HTTP2_HEADER_STATUS]: HTTP_STATUS_NOT_FOUND }, { endStream: true })
+		return
+	}
+
+	if (!ext) {
+		const fileName = `graphs_${name}.svg`
+		requestedPath = path.join(dir, '/graphs', fileName)
 	}
 	const fullPath = path.join(PUBLIC_ROOT, requestedPath)
 	const responseMimeType = mime.lookup(fullPath)
